1def14727512688a5150aaea7f9fe534b79af6e2
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-24 23:48:36 -0400

    add assets folder and navbar

---
+++ milos-bakery/src/app/navBar.tsx
@@ -0,0 +1,93 @@
+import "./navBar.css";
+import logo from "./assets/new logo.png";
+import Image from "next/image";
+// import ShoppingCart from "./shoppingCart";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary ">
+        <div className="container-fluid">
+          <a className="navbar-brand ms-5" href="#">
+            <Image src={logo} alt="Logo" width="80" height="80" />
+            <span className="brand-name">Milo&apos;s homemade</span>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex justify-content-center"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav  mb-2 mb-lg-0 ">
+              <li className="nav-item mx-4  text-center">
+                <a
+                  className="nav-link active text-info fw-semibold hover-change-color fs-5"
+                  aria-current="page"
+                  href="#hero-parent-id"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item mx-4  text-center">
+                <a
+                  className="nav-link text-info fw-semibold hover-change-color fs-5"
+                  href="#our-menu"
+                >
+                  Our menu
+                </a>
+              </li>
+
+              <li className="nav-item mx-4  text-center">
+                <a
+                  className="nav-link text-info fw-semibold hover-change-color fs-5"
+                  href="#gallery-title"
+                >
+                  Gallery
+                </a>
+              </li>
+
+              <li className="nav-item mx-4  text-center">
+                <a
+                  className="nav-link text-info fw-semibold hover-change-color fs-5"
+                  href="#contacts"
+                >
+                  Contacts
+                </a>
+              </li>
+            </ul>
+
+            {/* <button
+              type="button"
+              className=" shopping-cart d-flex me-5"
+              data-bs-toggle="modal"
+              data-bs-target="#shoppingCartModal"
+            >
+              <a
+                className="cart my-auto mx-2 text-info fw-bolder fs-5"
+                href="#"
+              >
+                Cart
+              </a>
+
+              <i
+                className="bi bi-cart "
+                style={{ fontSize: "2.5rem", color: "green" }}
+              ></i>
+            </button> */}
+          </div>
+        </div>
+      </nav>
+
+      {/* <ShoppingCart /> */}
+    </>
+  );
+}
