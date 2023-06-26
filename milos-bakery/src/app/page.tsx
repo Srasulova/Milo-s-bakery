e5bb658ca7d588225cc3dda3a0108f3c2bf11240
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-26 01:46:39 -0400

    update the page with the changes

--- milos-bakery/src/app/page.tsx
+++ milos-bakery/src/app/page.tsx
@@ -3,11 +3,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from "./navBar";
+import HeroArea from "./heroArea";
+import Footer from "./footer";
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
+      <HeroArea />
+      <Footer />
     </main>
   );
 }
