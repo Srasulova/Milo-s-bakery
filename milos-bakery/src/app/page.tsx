b414ecdec8f8ef5235c1dc0fbf7e969e7525d879
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-06-27 02:29:52 -0400

    update page with whatWeMake

--- milos-bakery/src/app/page.tsx
+++ milos-bakery/src/app/page.tsx
@@ -5,12 +5,14 @@ import styles from "./page.module.css";
 import NavBar from "./navBar";
 import HeroArea from "./heroArea";
 import Footer from "./footer";
+import WhatWeMake from "./whatWeMake";
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
       <HeroArea />
+      <WhatWeMake />
       <Footer />
     </main>
   );
